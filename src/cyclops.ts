0a1b69e0389fbe4b620d6ef57357b646a39dda73
Charles ⚡ <charlespeters42@gmail.com>
2019-02-24 01:21:28 -0800

    more tests and no faux debug

--- src/cyclops.ts
+++ src/cyclops.ts
@@ -2,30 +2,14 @@ interface CyclopsEvent {
   [key: string]: any[];
 }
 
-interface CyclopsOptions {
-  debug?: true;
-}
-
- /**
-  * Adapted from 'https://gist.github.com/kutyel/7d8f204a347840a6ee7220743957e504'
-  */
-
-const debug = (title: string): string => `|--- ${title} ---|`;
-
 class Cyclops {
   events: CyclopsEvent;
-  debug?: boolean;
-  public constructor(events = {}, options: CyclopsOptions) {
-    this.events = events;
-    this.debug = options.debug;
+  constructor(events?: CyclopsEvent) {
+    this.events = events || {};
   }
 
   public subscribe(name: string, cb: Function) {
     (this.events[name] || (this.events[name] = [])).push(cb);
-
-    if (this.debug) {
-      console.log(debug("EVENTS"), this.events);
-    }
 
     return {
       release: () =>
@@ -35,11 +19,7 @@ class Cyclops {
   }
 
   public emit(name: string, ...args: any[]) {
-    if (this.debug) {
-      console.log(debug("EVENTS"), name, this.events[name].map(fn => fn));
-    }
-
-    return (this.events[name] || []).map(fn => fn(...args));
+    (this.events[name] || []).forEach(fn => fn(...args));
   }
 }
 export default Cyclops;
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,10 @@
-import Cyclops from "./cyclops";
+import EventEmitter from "./cyclops";
 
 const mock = jest.fn();
 
 describe("Cyclops", () => {
   it("emits result when fired", () => {
-    const m = new Cyclops({}, { debug: true });
+    const m = new EventEmitter({});
     let val = 0;
 
     m.subscribe("mock event", (n: number) => (val = n));
@@ -13,8 +13,19 @@ describe("Cyclops", () => {
     expect(val).toBe(18);
   });
 
+  it("contains event names", () => {
+    const m = new EventEmitter({});
+
+    m.subscribe("mock event", mock);
+    m.subscribe("other mock event", mock);
+    const events = Object.keys(m.events);
+
+    expect(events).toContain("mock event");
+    expect(events).toContain("other mock event");
+  });
+
   it("fires call back when emitted", () => {
-    const m = new Cyclops({}, { debug: true });
+    const m = new EventEmitter({});
     let val = 0;
 
     m.subscribe("mock event", (n: number) => mock(n));
@@ -22,5 +33,23 @@ describe("Cyclops", () => {
 
     expect(mock).toHaveBeenCalled();
   });
+
+  it("unsubscribes from event", () => {
+    const m = new EventEmitter();
+
+    let val = 0;
+
+    const e = m.subscribe("mock event", (n: number) => (val = n));
+    m.subscribe("other mock event", mock);
+    m.emit("mock event", 10);
+    expect(val).toBe(10);
+
+    e.release();
+
+    m.emit("mock event", 18);
+    expect(val).not.toBe(18);
+    expect(m.events["mock event"]).toHaveLength(0);
+  });
+
   xit("fires event", () => {});
 });
